ccce8d75018283be2fbab157798d124f3640ddf0
Kuralay-02 <72wsdydm8d@privaterelay.appleid.com>
2026-01-21 15:26:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,25 +8,21 @@ dp = Dispatcher(bot)
 
 users = {}
 
-@dp.message_handler(commands=['start'])
-async def start(msg: types.Message):
-    if not msg.from_user.username:
-        await msg.answer("Нужен username в Telegram.")
-        return
-    users[msg.from_user.username.lower()] = msg.from_user.id
-    await msg.answer("Я буду уведомлять тебя при упоминании.")
-
 @dp.channel_post_handler()
 async def channel_post(message: types.Message):
     if not message.text:
         return
+
     text = message.text.lower()
+
+    channel_username = message.chat.username
+    post_id = message.message_id
+    post_link = f"https://t.me/{channel_username}/{post_id}"
+
     for username, user_id in users.items():
         if f"@{username}" in text:
             await bot.send_message(
                 user_id,
-                f"Тебя упомянули:\n\n{message.text}"
+                f"Вас упомянули в посте в Джурыми! \n{post_link}"
             )
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
